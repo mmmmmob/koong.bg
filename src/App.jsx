94f26dbb90d27c7459ce6b6beabf7b555d58023d
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-06-27 01:53:53 +0700

    add: install @vercel analytics component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { Analytics } from "@vercel/analytics/react";
+
 function App() {
   return (
-    <div className="flex h-screen w-screen justify-center">
-      <h1 className="self-center text-6xl font-bold max-sm:text-4xl">
-        🚧 Coming Soon 🚧
-      </h1>
-    </div>
+    <>
+      <div className="flex h-screen w-screen justify-center">
+        <h1 className="self-center text-6xl font-bold max-sm:text-4xl">
+          🚧 Coming Soon 🚧
+        </h1>
+      </div>
+      <Analytics />
+    </>
   );
 }
 
