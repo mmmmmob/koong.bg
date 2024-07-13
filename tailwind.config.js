eed6771423c77c75409e27a74688c4cca0814bae
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-07-13 14:42:22 +0700

    style: update typeface families

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Komika Hand"],
+        serif: ["Lora"],
+      },
+    },
   },
   plugins: [],
 };
