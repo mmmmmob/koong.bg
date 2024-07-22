6c7af98d5641565da8540830ac04ed8fc864a9f0
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-07-22 23:42:25 +0700

    style: draft layout for about modal

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
 
   return (
     <>
+      {/* about modal */}
       <Modal
         isOpen={aboutModal.isOpen}
         id="1"
@@ -29,32 +30,50 @@ function App() {
         className="mx-4"
       >
         <ModalContent>
-          {(onClose) => (
+          {() => (
             <>
-              <ModalHeader className="flex flex-col gap-1">About</ModalHeader>
-              <ModalBody className="my-2">
-                <ScrollShadow size={100}>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Nullam pulvinar risus non risus hendrerit venenatis.
-                    Pellentesque sit amet hendrerit risus, sed porttitor quam.
-                  </p>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Nullam pulvinar risus non risus hendrerit venenatis.
-                    Pellentesque sit amet hendrerit risus, sed porttitor quam.
-                  </p>
-
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Nullam pulvinar risus non risus hendrerit venenatis.
-                    Pellentesque sit amet hendrerit risus, sed porttitor quam.
-                  </p>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Nullam pulvinar risus non risus hendrerit venenatis.
-                    Pellentesque sit amet hendrerit risus, sed porttitor quam.
-                  </p>
+              <ModalHeader className="mt-3 flex flex-col gap-1 font-serif text-3xl">
+                About
+              </ModalHeader>
+              <ModalBody className="my-2 font-serif text-lg font-thin">
+                <ScrollShadow size={50}>
+                  <div className="flex max-sm:flex-col sm:h-64">
+                    <div className="flex-shrink-0">
+                      <img
+                        src="/public/favicon.jpg"
+                        alt="avatar"
+                        className="overflow-hidden sm:size-64"
+                      />
+                    </div>
+                    <ScrollShadow size={50}>
+                      <div className="max-sm:my-3 sm:mx-3">
+                        <p>
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Vero, optio! Expedita voluptatibus animi ad
+                          illum aperiam temporibus nihil, quo deleniti, hic modi
+                          corrupti laudantium ea cum quas ut doloremque illo.
+                        </p>
+                        <p>
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Vero, optio! Expedita voluptatibus animi ad
+                          illum aperiam temporibus nihil, quo deleniti, hic modi
+                          corrupti laudantium ea cum quas ut doloremque illo.
+                        </p>
+                        <p>
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Vero, optio! Expedita voluptatibus animi ad
+                          illum aperiam temporibus nihil, quo deleniti, hic modi
+                          corrupti laudantium ea cum quas ut doloremque illo.
+                        </p>
+                        <p>
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Vero, optio! Expedita voluptatibus animi ad
+                          illum aperiam temporibus nihil, quo deleniti, hic modi
+                          corrupti laudantium ea cum quas ut doloremque illo.
+                        </p>
+                      </div>
+                    </ScrollShadow>
+                  </div>
                 </ScrollShadow>
               </ModalBody>
               <ModalFooter></ModalFooter>
@@ -63,6 +82,7 @@ function App() {
         </ModalContent>
       </Modal>
 
+      {/* works modal */}
       <Modal
         isOpen={worksModal.isOpen}
         backdrop="blur"
@@ -74,10 +94,12 @@ function App() {
         className="mx-4"
       >
         <ModalContent>
-          {(onClose) => (
+          {() => (
             <>
-              <ModalHeader className="flex flex-col gap-1">Works</ModalHeader>
-              <ModalBody className="my-2">
+              <ModalHeader className="mt-3 flex flex-col gap-1 font-serif text-3xl">
+                Works
+              </ModalHeader>
+              <ModalBody className="my-2 font-serif text-lg font-thin">
                 <ScrollShadow size={100}>
                   <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit.
@@ -108,6 +130,7 @@ function App() {
         </ModalContent>
       </Modal>
 
+      {/* shops modal */}
       <Modal
         isOpen={shopsModal.isOpen}
         backdrop="blur"
@@ -119,10 +142,12 @@ function App() {
         className="mx-4"
       >
         <ModalContent>
-          {(onClose) => (
+          {() => (
             <>
-              <ModalHeader className="flex flex-col gap-1">Shops</ModalHeader>
-              <ModalBody className="my-2">
+              <ModalHeader className="mt-3 flex flex-col gap-1 font-serif text-3xl">
+                Shops
+              </ModalHeader>
+              <ModalBody className="my-2 font-serif text-lg font-thin">
                 <ScrollShadow size={100}>
                   <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit.
@@ -154,33 +179,33 @@ function App() {
       </Modal>
 
       <div className="bg-color-main text-color-main flex h-[calc(100svh-56px)] w-screen items-center justify-evenly self-center max-sm:flex-col">
-        {/* <a
-          //onClick={aboutModal.onOpen}
+        <a
+          onClick={aboutModal.onOpen}
           className="font-serif text-2xl hover:underline max-sm:text-xl"
         >
           about
         </a>
         <a
-          //onClick={worksModal.onOpen}
+          onClick={worksModal.onOpen}
           className="font-serif text-2xl hover:underline max-sm:text-xl"
         >
           works
-        </a> */}
+        </a>
         <h1 className="font-sans text-6xl font-bold italic tracking-wider max-sm:text-5xl">
           KOONG
         </h1>
-        {/* <a
-          //onClick={shopsModal.onOpen}
+        <a
+          onClick={shopsModal.onOpen}
           className="font-serif text-2xl hover:underline max-sm:text-xl"
         >
           shops
-        </a> */}
-        {/* <a
+        </a>
+        <a
           href="mailto:koong.bg@gmail.com"
           className="font-serif text-2xl hover:underline max-sm:text-xl"
         >
           contact
-        </a> */}
+        </a>
       </div>
       <footer className="sticky bottom-0">
         <Footer />
