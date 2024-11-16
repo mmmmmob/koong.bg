32f56bf270f9346973ea9c17027b1a494bc19255
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-11-16 21:42:34 +0700

    style: add Bluesky icon and link to profile

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,27 @@
 import { FaFacebook, FaInstagram } from "react-icons/fa";
 import { FaPatreon, FaTiktok, FaXTwitter } from "react-icons/fa6";
 import { SiWebtoon } from "react-icons/si";
+import { SiBluesky } from "react-icons/si";
 
 function Footer() {
   return (
-    <div className="flex h-14 w-screen items-center justify-center gap-14 bg-stone-700 text-stone-200 dark:bg-zinc-700 dark:text-stone-200 max-sm:gap-10">
+    <div
+      className="flex h-14 w-screen items-center justify-center gap-14 bg-stone-700 text-stone-200 dark:bg-zinc-700 dark:text-stone-200 max-sm:gap-7">
+      {/* Patreon */}
       <a href="https://www.patreon.com/koongbg" target="_blank">
         <FaPatreon size={25} />
       </a>
+      {/* Webtoon */}
       <a
         href="https://www.webtoons.com/en/canvas/pointillism/list?title_no=835155"
         target="_blank"
       >
         <SiWebtoon size={25} />
       </a>
-      {/* Facebook */}
-      <a href="https://www.facebook.com/koongbg" target="_blank">
-        <FaFacebook size={25} />
+      {/* Bluesky */}
+      <a href="https://bsky.app/profile/koong.bg" target="_blank">
+        <SiBluesky size={25} />
       </a>
-
       {/* Instagram */}
       <a href="https://www.instagram.com/koong.bg" target="_blank">
         <FaInstagram size={26} />
@@ -30,6 +33,10 @@ function Footer() {
       {/* TikTok */}
       <a href="https://www.tiktok.com/@koongbg" target="_blank">
         <FaTiktok size={24} />
+      </a>
+      {/* Facebook */}
+      <a href="https://www.facebook.com/koongbg" target="_blank">
+        <FaFacebook size={25} />
       </a>
     </div>
   );
