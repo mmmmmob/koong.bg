ad9b53f02fcb3d109a09617b1da3b7415c56d2e8
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-12-07 22:26:15 +0700

    chore: remove unused nextui component

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import { NextUIProvider } from "@nextui-org/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
@@ -14,10 +13,6 @@ const theme = () => {
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <NextUIProvider>
-      <main className={theme()}>
         <App />
-      </main>
-    </NextUIProvider>
   </React.StrictMode>,
 );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,6 @@
-// eslint-disable-next-line no-undef
-const { nextui } = require("@nextui-org/react");
-
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
@@ -24,8 +17,6 @@ export default {
       backgroundSize: {
         "200%": "200%",
       },
-      darkMode: "class",
-      plugins: [nextui()],
     },
   },
 };
